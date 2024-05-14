ab6accef57cad3284ae1991fd1c026f122e5d672
atique <atiquevellore@gmail.com>
2024-05-14 07:19:32 +0530

    test case 1

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,2 +1,12 @@
 class StringCalculator
+    def self.add(numbers)
+       sum = 0;
+       if numbers.strip.empty?
+            0
+       else
+            numbers.split(",").map{|num| sum+=num.to_i}
+            sum
+       end
+
+    end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,4 +1,14 @@
 require "string_calculator"
 describe StringCalculator do
+    context "add method can take up to two numbers" do
+        it "returns their sum" do
+            expect(StringCalculator.add("1,2")).to eq(3)  
+        end
+
+        it "returns 0 if the numbers is an empty string" do 
+            expect(StringCalculator.add("    ")).to eq(0)
+        end
+    end
+
     
 end
